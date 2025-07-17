9ef0221aa2cc83eec8fbda8e02c1c0add3283aac
Alexsam14 <samalex460@gmail.com>
2025-07-17 09:47:30 +0000

    making changes

--- PHY/rtl/shift_reg.v
+++ PHY/rtl/shift_reg.v
@@ -1,16 +1,16 @@
 module shift_reg_8bit (
     input clk,
     input reset,
-    input enable,
+    input shift_enable,
     input reg serial_in,
     output reg [7:0] parallel_out;
 );
 always @(posedge clk) begin
     if (reset) begin
-        Q<=8'b0;
+        parallel_out<=8'b0;
     end
-    else begin
-        Q<={serial_in, parallel_out[7:1]};
+    else if (shift_enable) begin
+        parallel_out<={serial_in, parallel_out[7:1]};
     end
 end
     
